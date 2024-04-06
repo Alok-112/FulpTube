39024d4419e399b83b940063b04e589d5e16d8a4
alok-112 <a.thakurm007@.com>
2024-04-06 11:07:42 +0530

    dotenv

---
+++ src/config.js
@@ -0,0 +1,4 @@
+// config.js
+const API_KEY = 'AIzaSyAxMRxshEh3_VtrMGIiyQ3jF4i37Y5M9d4';
+
+export { API_KEY };
--- src/data.js
+++ src/data.js
@@ -1,16 +1,14 @@
-export const API_KEY = "AIzaSyAxMRxshEh3_VtrMGIiyQ3jF4i37Y5M9d4";   
+export const API_KEY = 'AIzaSyAxMRxshEh3_VtrMGIiyQ3jF4i37Y5M9d4'
+
 
 export const value_converter = (value) => {
-    if(value>=1000000)
-    {
-        return Math.floor(value/1000000)+"M";
+    if (value >= 1000000) {
+        return Math.floor(value / 1000000) + "M";
     }
-    else if(value>=1000)
-    {
-        return Math.floor(value/1000)+"K";
+    else if (value >= 1000) {
+        return Math.floor(value / 1000) + "K";
     }
-    else
-    {
+    else {
         return value;
     }
 }
